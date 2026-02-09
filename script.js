858f7332fa268271093ebc841ae374b8b66c5bdc
trieuvidev-crypto <doriyeuem2005@gmail.com>
2026-02-09 15:33:14 +0700

    Update script.js
    Signed-off-by: trieuvidev-crypto <doriyeuem2005@gmail.com>

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const menuData = {
         { id: 30, name: "Trà sữa socola", price: 20000 }
     ],
     "ĂN VẶT": [
-        { id: 31, name: "Bánh tráng trộn", price: 15000 },
+        { id: 31, name: "Bánh tráng trộn", price: 20000 },
         { id: 32, name: "Bò viên chiên", price: 15000 },
         { id: 33, name: "Cá viên chiên", price: 15000 },
         { id: 34, name: "Trái cây ly", price: 10000 },
